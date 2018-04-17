7a6be826420c9bdb2357a9cfa42373cb224b60b5
tideee2 <gntnau@gmail.com>
2018-04-17 13:47:20 +0300

    http task

---
+++ day1/http/server.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var querystring = require('querystring');
+
+var server = http.createServer().listen(3000);
+
+server.on('request', function (req, res) {
+    if (req.method == 'POST') {
+        var body = '';
+    }
+
+    req.on('data', function (data) {
+        body += data;
+    });
+
+    req.on('end', function () {
+        var post = querystring.parse(body);
+        console.log(post);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<html><body><h1>Hello, World!</h1></body></html>');
+    });
+});
+
+console.log('Listening on port 3000');
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'a'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
